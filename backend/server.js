5133eb9ef0fa1af6d2150732306f40d82cdb172f
Chandu Dhondi <chandudhondi@Chandus-Laptop.local>
2025-07-30 14:17:25 +0530

    final work

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://cyberminds-chandu.netlify.app"],
+    origin: ["https://cyberminds-chandu.netlify.app", "http://localhost:5173"],
     methods: ["GET", "POST"],
   },
 });
@@ -19,7 +19,7 @@ setupSocket(io);
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://cyberminds-chandu.netlify.app"],
+    origin: ["https://cyberminds-chandu.netlify.app", "http://localhost:5173"],
     methods: ["GET", "POST"],
     credentials: true,
   })
