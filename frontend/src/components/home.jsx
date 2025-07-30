030f5499b654683f5cb23a2607b9b7d2e59e8445
Chandu Dhondi <chandudhondi@Chandus-Laptop.local>
2025-07-30 14:15:48 +0530

    final work

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -4,7 +4,7 @@ import JobForm from "./jobForm";
 import { io } from "socket.io-client";
 import JobCard from "./jobCard";
 
-const socket = io("http://localhost:8080");
+const socket = io("https://cyberminds-chandu.onrender.com");
 
 export default function Home() {
   const [showCreateJob, setShowCreateJob] = useState(false);
